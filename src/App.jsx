671df94174304965d530d5d0376ac388e4449fce
EnzoETR <enzo.torres@estudiantes.utec.edu.uy>
2026-05-11 09:52:45 -0300

    paginas,navegacion,routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import BarraNav from "./components/BarraNav";
+import Footer from "./components/Footer";
+
 import Inicio from "./pages/Inicio";
+import Catalogo from "./pages/Catalogo";
+import Pedido from "./pages/Pedido";
+import Contacto from "./pages/Contacto";
+import MiCuenta from "./pages/MiCuenta";
 
 function App() {
-  return <Inicio />;
+  return (
+    <BrowserRouter>
 
+      <BarraNav />
+
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/catalogo" element={<Catalogo />} />
+        <Route path="/pedido" element={<Pedido />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/micuenta" element={<MiCuenta />} />
+      </Routes>
+
+       <Footer />
+
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,11 +1,11 @@
 import BarraNav from "../components/BarraNav";
 import Footer from "../components/Footer";
 
-export default function Inicio() {
+export default function Catalogo() {
   return (
     <div className="min-h-screen flex flex-col">
       <main className="flex-grow p-6">
-        <h2>Página principal</h2>
+        <h2>Catalogo</h2>
       </main>
     </div>
   );
